19e38b96d563e59775808d754152e7529d847f39
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2025-07-22 11:07:52 +0600

    Add GTK folder picker support
    Introduces GtkFolderPicker for Linux platforms using GTK 4 and optional Libadwaita. Updates dependency management to include PyGObject as an optional dependency and refactors initialization to support both Qt and GTK folder pickers.

--- src/cross_platform_folder_picker/bases/__init__.py
+++ src/cross_platform_folder_picker/bases/__init__.py
@@ -1,3 +1,6 @@
 from .windows import WindowsFolderPicker as WindowsFolderPicker
 from .linux import LinuxFolderPicker as LinuxFolderPicker
 from .macos import MacOSFolderPicker as MacOSFolderPicker
+
+from .qt import QtFolderPicker as QtFolderPicker
+from .gtk import GtkFolderPicker as GtkFolderPicker
